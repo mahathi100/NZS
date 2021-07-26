4cce6386e159d45baf88ae66cfae55982587a055
mahathi100 <87945501+mahathi100@users.noreply.github.com>
2021-07-26 13:15:50 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$email_form = 'mahathirmohammad444@gmail.com';
+
+$email_subject = 'New from Submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n". 
+                "Subject: $subject.\n". 
+                "User Message: $message.\n";
+
+$to = 'mahathipol098@gmail.com';
+
+$headers = "From: $email_form \r\n";
+
+$headers .= "Reply-to: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+
+
+
+
+
+
+
+?>
